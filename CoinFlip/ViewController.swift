3f2d9a09ef56cb47f884cb9a06c58a39d9a48612
Michelle379 <michelle379123@gmail.com>
2024-09-03 19:25:47 +0800

    Initial Commit

---
+++ CoinFlip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinFlip
+//
+//  Created by 王宜婕 on 2024/9/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
